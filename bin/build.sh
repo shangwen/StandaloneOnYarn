2dec115576fab8f273a6be9bbbc5daafa5ac6c7c
tangshangwen <tangshangwen@jd.com>
2017-05-25 15:59:43 +0800

    fixed build

--- bin/build.sh
+++ bin/build.sh
@@ -8,7 +8,7 @@ cd $STANDALONE_ON_YARN_HOME
 mvn clean package
 rm -rf ./build
 mkdir -p ./build/standalone-yarn-0.1/
-cp -a ./target/SparkOnYarn-1.0-SNAPSHOT-jar-with-dependencies.jar ./build/standalone-yarn-0.1/standalone-yarn-0.1.jar
+cp -a ./target/standalone-on-yarn-1.0-SNAPSHOT-jar-with-dependencies.jar ./build/standalone-yarn-0.1/standalone-yarn-0.1.jar
 cp -a ./bin ./build/standalone-yarn-0.1/
 cp -a ./etc ./build/standalone-yarn-0.1/
 cd ./build/standalone-yarn-0.1/
